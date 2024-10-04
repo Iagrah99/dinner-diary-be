3fb18e360354db6b7a22be09b43c781f3970b3e2
Ian Graham <i4ng1999@gmail.com>
2024-10-04 20:27:50 +0100

    Populate example test data

---
+++ db/data/test-data/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    email: 'mealplanner_mom@example.com',
+    username: 'MealPlannerMom',
+    password: 'strong_password123',
+  },
+  {
+    email: 'fitness_fanatic@example.com',
+    username: 'FitnessFanatic',
+    password: 'secure_fitness123',
+  },
+  {
+    email: 'student_foodie@example.com',
+    username: 'StudentFoodie',
+    password: 'unique_password123',
+  },
+];
